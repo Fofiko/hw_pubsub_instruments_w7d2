adb4a63829fead99e0770c3a22772b9f4e8e6c9e
Fofiko <foteinid@gmail>
2018-09-12 08:13:54 +0100

    Refactor instrument view code to implement small methods responsible creating sections of the UI

--- hw_pubsub_instruments_start/src/views/instrument_info_view.js
+++ hw_pubsub_instruments_start/src/views/instrument_info_view.js
@@ -14,12 +14,39 @@ InstrumentInfoView.prototype.bindEvents = function(){
 
 InstrumentInfoView.prototype.render = function(family){
   this.container.innerHTML = '';
-  const infoParagraph = document.createElement('p');
-  infoParagraph.textContent = `${family.name}\r\n ${family.description}\r\n Instruments include: ${family.instruments}.`;
-  // console.log("infoParagraph:", infoParagraph);
-  this.container.appendChild(infoParagraph);
+
+  // const infoParagraph = document.createElement('p');
+  // infoParagraph.textContent = `${family.name}\r\n ${family.description}\r\n Instruments include: ${family.instruments}.`;
+  // // console.log("infoParagraph:", infoParagraph);
+  // this.container.appendChild(infoParagraph);
+
+  const heading = this.createHeading(family);
+  const bodyCopy = this.createBodyCopy(family);
+  const instrument_list = this.createInstrumentList(family);
+
+  this.container.appendChild(heading);
+  this.container.appendChild(bodyCopy);
+  this.container.appendChild(instrument_list);
 };
 
+InstrumentInfoView.prototype.createHeading = function (family) {
+  const heading = document.createElement('h2');
+  heading.textContent = family.name;
+  return heading;
+};
+
+InstrumentInfoView.prototype.createBodyCopy = function (family) {
+  const bodyCopy = document.createElement('p');
+  bodyCopy.textContent = family.description;
+  return bodyCopy;
+
+};
+
+InstrumentInfoView.prototype.createInstrumentList = function (family) {
+  const instrument_list = document.createElement('ul');
+  instrument_list.textContent = `Instruments include: ${family.instruments}`;
+  return instrument_list;
+};
 
 
 module.exports = InstrumentInfoView;
